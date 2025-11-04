a036f0b9e2d94cd73116bc0b347e56401bcce3a8
sysgenhousegg <87147041+sysgenhousegg@users.noreply.github.com>
2025-11-04 17:32:22 -0500

    Initial commit

---
+++ src/main/java/com/linktic/tecnica/ptecnica_back/PtecnicaBackApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.tecnica.ptecnica_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtecnicaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtecnicaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/tecnica/ptecnica_back/PtecnicaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.tecnica.ptecnica_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtecnicaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
